3b7b97b0193584073cb4284a3878f36f69ad8cb6
erick <erickfloresgomez5@gmail.com>
2023-01-18 11:39:54 -0600

    Commit de ejercicio tema 1

---
+++ src/NuevoPaquete/NuevaClase.java
@@ -0,0 +1,38 @@
+package NuevoPaquete;
+
+public class NuevaClase {
+    public static void main(String[] args) {
+        //Tipos de dato numérico
+        byte numero1 = 1;
+        short numero2 = 2;
+        int numero3 = 3;
+        long numero4 = 4;
+
+        //Tipos de datos con decimales
+        double numero5 = 5.5d;
+        float numero6 = 6.5f;
+
+        // Tipos de dato booleanos
+        boolean false1 = false;
+        boolean true1 = true;
+
+        //Tipos de datos de texto
+        char texto1 = 'e';
+        String texto2 = "Texto2";
+
+        System.out.println("Tipos de datos en Java \n"+"Numericos\n"+
+                "byte :"+numero1+"\n"+
+                "short :"+numero2+"\n"+
+                "int :"+numero3+"\n"+
+                "long :"+numero4+"\n"+
+                "double :"+numero5+"\n"+
+                "float :"+numero6+"\n"+
+                "Tipos de datos booleanos\n"+
+                false1+" "+ true1 +"\n"+
+                "Tipos de datos de Texto\n"+
+                "char :"+ texto1 + "\n"+
+                "String :"+ texto2
+                );
+
+    }
+}
